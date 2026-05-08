a32ec09ef313e459a81c88e064cd5fb9f7ba5735
Shoham <asafov.shoham@gmail.com>
2026-05-08 16:01:20 +0300

    fix: remove allowedOrigins

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000'],
-    },
-  },
 }
 
 module.exports = nextConfig
